e5d3aef8c1eddffcc2811cbd09c6649f05f45f21
viniciusxande30 <vinicius.xande30@gmail.com>
2023-10-03 20:56:22 -0300

    update

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,113 @@
+        <!-- main-area-end -->
+        <!-- footer -->
+        <footer class="footer-bg footer-p">
+            <div class="footer-top  pt-70  pb-40">
+                <div class="container">
+                    <div class="row justify-content-between">
+                        
+                          <div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title mb-30">
+                                   <img src="{{url('/')}}/assets/img/logo/f_logo.png" alt="img">
+                                </div>
+                                <div class="f-contact">
+                                    <ul>
+                                    <li>
+                                        <i class="icon fal fa-phone"></i>
+                                        <span>+55(11) 2638-9710</span>
+                                    </li>
+                                   <li><i class="icon fal fa-envelope"></i>
+                                        <span>
+                                       <a href="mailto:contato@signadvisory.com.br">contato@signadvisory.com.br</a>
+                                       </span>
+                                    </li>
+                                    <li>
+                                        <i class="icon fal fa-map-marker-check"></i>
+                                        <span>Rua Helena, 285 Cjs. 43/45<br> - Vila Olimpia - SP</span>
+                                    </li>
+                                    
+                                </ul>
+                                    
+                                    </div>
+                            </div>
+                        </div>
+						<div class="col-xl-2 col-lg-2 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                    <h2>Nossas Páginas</h2>
+                                </div>
+                                <div class="footer-link">
+                                    <ul>                                        
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html"> Quem Somos</a></li>
+                                        <li><a href="services.html"> Clientes </a></li>
+                                        <li><a href="contact.html"> Serviços</a></li>
+                                        <li><a href="blog.html">Contato </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-2 col-lg-2 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                    <h2>Nossos Serviços</h2>
+                                </div>
+                                <div class="footer-link">
+                                    <ul>
+                                        <li><a href="faq.html">Auditoria</a></li>
+                                        <li><a href="#">Tributário</a></li>
+                                        <li><a href="#">Consultoria</a></li>
+                                        <li><a href="#">Consultoria de RH</a></li>
+                                        <li><a href="#">BPS</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>  
+                      
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-wrap">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6">                         
+                               Copyright  © 2023 Sign Audit & Advisory. Todos os Direitos Reservados.                       
+                        </div>
+                        <div class="col-lg-6 text-right text-xl-right">                       
+                            <ul>
+                                <li><a href="#">Termos de Privacidade</a></li>
+                               
+                            </ul>                 
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer-end -->
+
+
+		<!-- JS here -->
+        <script src="{{url('/')}}/assets/js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="{{url('/')}}/assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="{{url('/')}}/assets/js/popper.min.js"></script>
+        <script src="{{url('/')}}/assets/js/bootstrap.min.js"></script>
+        <script src="{{url('/')}}/assets/js/one-page-nav-min.js"></script>
+        <script src="{{url('/')}}/assets/js/slick.min.js"></script>
+        <script src="{{url('/')}}/assets/js/ajax-form.js"></script>
+        <script src="{{url('/')}}/assets/js/paroller.js"></script>
+        <script src="{{url('/')}}/assets/js/wow.min.js"></script>
+        <script src="{{url('/')}}/assets/js/js_isotope.pkgd.min.js"></script>
+        <script src="{{url('/')}}/assets/js/imagesloaded.min.js"></script>
+        <script src="{{url('/')}}/assets/js/parallax.min.js"></script>
+        <script src="{{url('/')}}/assets/js/jquery.waypoints.min.js"></script>
+        <script src="{{url('/')}}/assets/js/jquery.counterup.min.js"></script>
+        <script src="{{url('/')}}/assets/js/jquery.scrollUp.min.js"></script>
+        <script src="{{url('/')}}/assets/js/jquery.meanmenu.min.js"></script>
+        <script src="{{url('/')}}/assets/js/parallax-scroll.js"></script>
+        <script src="{{url('/')}}/assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="{{url('/')}}/assets/js/element-in-view.js"></script>
+        <script src="{{url('/')}}/assets/js/main.js"></script>
+    </body>
+</html>
---
+++ resources/views/includes/top.blade.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Sign Audit & Advisory</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+
+		<!-- CSS here -->
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/animate.min.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/fontawesome/css/all.min.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/font-flaticon/flaticon.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/dripicons.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/slick.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/meanmenu.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/default.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/style.css">
+        <link rel="stylesheet" href="{{url('/')}}/assets/css/responsive.css">
+    </head>
+    <body>
+     <!-- header -->
+        <header class="header-area header-area2">  
+			<div class="header-top second-header d-none d-lg-block">
+                <div class="container">
+                    <div class="row align-items-center">                                           
+                        <div class="col-lg-4 col-md-4 d-none d-lg-block">
+                             <div class="header-social">
+                            <span>
+                                <a href="#" title="Facebook"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                                
+                               </span>                    
+                               <!--  /social media icon redux -->                               
+                        </div>
+                        </div>
+                        <div class="col-lg-8 col-md-8 d-none  d-md-block">
+                            <div class="header-cta">
+                                <ul>                                   
+                                    <li>
+                                        <div class="text">
+                                             <i class="fal fa-map-marker-check"></i> <div class="box"><strong>Rua Helena, 285 Cjs. 43/45</strong>  - Vila Olimpia - SP</div>
+                                        </div>
+                                      
+                                    </li>
+                                     <li>
+                                           <div class="text">
+                                             <i class="far fa-phone-alt"></i> <div class="box"><strong>Fone</strong>+55(11) 2638-9710</div>
+                                        </div>
+                                      
+                                    </li>
+                                    <li>
+                                         <div class="text">
+                                             <i class="fal fa-clock"></i><div class="box"><strong>E-mail</strong> contato@signadvisory.com.br</div>
+                                        </div>
+                                      
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                         
+                        
+                    </div>
+                </div>
+            </div>			
+            <div id="header-sticky" class="menu-area">
+                <div class="container">
+                    <div class="second-menu">
+                        <div class="row align-items-center">
+                            <div class="col-xl-2 col-lg-2">
+                                <div class="logo">
+                                    <a href="index.html"><img src="{{url('/')}}/assets/img/logo/logo.png" alt="logo"></a>
+                                </div>
+                            </div>
+                           <div class="col-xl-8 col-lg-8">
+                              
+                                <div class="main-menu text-right text-xl-right">
+                                    <nav id="mobile-menu" style="display: block;">
+                                        <ul>
+                                            <li>
+												<a href="index.html">Home</a>
+											
+											</li>
+                                            <li><a href="about.html">Quem Somos</a></li>
+                                            <li><a href="about.html">Clientes</a></li>
+                                            <li class="has-sub"> 
+                                                <a href="blog.html">Serviços</a>
+                                                <ul>
+                                                    <li><a href="blog.html">Auditoria</a></li>
+                                                    <li><a href="blog-details.html">Tributário</a></li>
+                                                    <li><a href="blog-details.html">Consultoria</a></li>
+                                                    <li><a href="blog-details.html">Consultoria de RH</a></li>
+                                                    <li><a href="blog-details.html">BPS</a></li>
+
+												</ul>
+                                            </li>
+                                            <li><a href="about.html">Contato</a></li>
+
+                                                                                  
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>   
+                            <div class="col-xl-2 col-lg-2 text-right d-none d-lg-block mt-15 mb-15">
+                                <a href="contact.html" class="btn ss-btn">Fale Conosco</a>
+                            </div>
+                          
+                            
+                                <div class="col-12">
+                                    <div class="mobile-menu"></div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- header-end -->
+        <!-- offcanvas-area -->
+                <div class="offcanvas-menu">
+                <span class="menu-close"><i class="fas fa-times"></i></span>
+              <form role="search" method="get" id="searchform"   class="searchform" action="http://wordpress.zcube.in/xconsulta/">
+                                <input type="text" name="s" id="search" value="" placeholder="Search"  />
+                                <button><i class="fa fa-search"></i></button>
+                            </form>
+
+                    
+                    <div id="cssmenu3" class="menu-one-page-menu-container">
+                        <ul id="menu-one-page-menu-2" class="menu">
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="index.html">Home</a></li>
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="about.html">Quem Somos</a></li>
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="services.html">Clientes</a></li>
+                             <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="pricing.html">Serviços </a></li>
+                             <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="team.html">Contato </a></li>
+                            
+                        </ul>
+                    </div>  
+                    
+                    <div id="cssmenu2" class="menu-one-page-menu-container">
+                        <ul id="menu-one-page-menu-1" class="menu">
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="#home"><span>+55(11) 2638-9710</span></a></li>
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><a href="#howitwork"><span>contato@signadvisory.com.br</span></a></li>
+                        </ul>
+                    </div>                
+            </div>
+            <div class="offcanvas-overly"></div>
+                 <!-- offcanvas-end -->
+        <!-- main-area -->
+        <main>
+             
+              <!-- slider-area -->
+            <section id="home" class="slider-area fix p-relative">
+               
+                <div class="slider-active">
+				
+                    <div class="single-slider slider-bg d-flex align-items-center" style="background-image:url({{url('/')}}/assets/img/slider/slider_img01.png)">
+                        <div class="container">
+                            <div class="row">
+                                 <div class="col-lg-8">
+                                    <div class="slider-content s-slider-content text2 mb-100">
+     
+                                         <h2 data-animation="fadeInUp" data-delay=".4s">Time Especializado Para o Seu Negócio</h2>
+                                        <p data-animation="fadeInUp" data-delay=".6s">Somos Especialistas com mais de 25 anos de experiência em diversos segmentos de nossa economia, como biotecnologia, concessionárias de rodovias, dispositivos médicos, farmacêuticas, instituições financeiras, seguradoras, serviços e tecnologia.</p>
+                                        <div class="slider-btn mt-40">                                          
+                                            <a href="services.html" class="btn ss-btn active mr-15" data-animation="fadeInRight" data-delay=".8s">Nossos Serviços</a>
+                                              <a href="https://www.youtube.com/watch?v=gyGsPlt06bo" class="video-i popup-video" data-animation="fadeInUp" data-delay=".8s" style="animation-delay: 0.8s;" tabindex="0"><i class="fas fa-play"></i> Entre em Contato</a>	
+                                        </div>                                        
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                   
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="single-slider slider-bg d-flex align-items-center" style="background-image:url({{url('/')}}/assets/img/slider/slider_img02.png)">
+                        <div class="container">
+                           <div class="row">
+                                <div class="col-lg-8">
+                                    <div class="slider-content s-slider-content text2 mb-100">
+                                         
+                                         <h2 data-animation="fadeInUp" data-delay=".4s">Your Business Find 
+Better Solution</h2>
+                                        <p data-animation="fadeInUp" data-delay=".6s">Fusce suscipit, velit id scelerisque tempus, enim est congue orci, eu sodales turpis tellus non elit. Cras hendrerit auctor cursus.</p>
+                                        <div class="slider-btn mt-40">                                          
+                                            <a href="services.html" class="btn ss-btn active mr-15" data-animation="fadeInRight" data-delay=".8s">Nossos Serviços</a>
+                                              <a href="https://www.youtube.com/watch?v=gyGsPlt06bo" class="video-i popup-video" data-animation="fadeInUp" data-delay=".8s" style="animation-delay: 0.8s;" tabindex="0"><i class="fas fa-play"></i> Entre em Contato</a>	
+                                        </div>                                        
+                                    </div>
+                                </div>
+                                <div class="col-lg-3">
+                                   
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+  
+                    
+                    </div>
+                    
+               
+            </section>
